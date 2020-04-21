dceec238d56580c6d89db280ac96ddcf0fa92332
pankaj gusain <gusainpankaj1999@gmail.com>
2020-04-21 15:34:41 +0530

    first commit:create the directory structure and set up express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log('error while running the server: $(err)')
+    }
+    else{
+        console.log('server is up and running at port: $ {port} ');
+    }
+})

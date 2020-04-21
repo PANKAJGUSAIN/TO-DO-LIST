48f9bba60bb1695abc24300b4db3dbab8e47dc30
pankaj gusain <gusainpankaj1999@gmail.com>
2020-04-21 21:19:21 +0530

    create router file index

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
 const port=8000;
-
+//
+ app.use('/',require('./routes/index'))
+//
 app.listen(port,function(err){
     if(err){
-        console.log('error while running the server: $(err)')
+        console.log(`error while running the server: ${err}`)
     }
     else{
-        console.log('server is up and running at port: $ {port} ');
+        console.log(`server is up and running at port: ${port}`);
     }
 })

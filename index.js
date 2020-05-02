41d6e6910881e97302a73f80e631eed859438ca7
pankaj gusain <gusainpankaj1999@gmail.com>
2020-05-02 14:44:43 +0530

    create changes to css

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const port=8000;
 const path=require('path')
 //
  app.use('/',require('./routes/index'));
+
  app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+ app.set('views',path.join(__dirname,'views'));
  app.use(express.static('assets'));
 //
+
 app.listen(port,function(err){
     if(err){
         console.log(`error while running the server: ${err}`)

aeb94a293e0d3741f80672b197f4c7cef4033878
pankaj gusain <gusainpankaj1999@gmail.com>
2020-05-02 14:17:08 +0530

    create css

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const path=require('path')
 //
- app.use('/',require('./routes/index'))
+ app.use('/',require('./routes/index'));
+ app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+ app.use(express.static('assets'));
 //
 app.listen(port,function(err){
     if(err){
